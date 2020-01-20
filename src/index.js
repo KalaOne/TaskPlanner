90138d6bad5f065c28d9ba22730e2d3618804470
Kaloyan Valchev <kaloyan.vylchev@gmail.com>
2020-01-20 07:57:44 +0000

    React updated ver. for Firebase
    Added correct configuration for Firebase to be linked, since React has updated their docs and the tutorial was outdated.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import firebase from 'firebase/app'
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(firebase, fbConfig)
+    )
+);
 
-ReactDOM.render(<Provider store={store}><App /> </Provider>, document.getElementById('root'));
+const rrfProps = {
+    firebase,
+    config: fbConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance
+  };
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <App />
+        </ReactReduxFirebaseProvider>
+    </Provider>,
+    document.getElementById('root'));
 serviceWorker.unregister();
